bd9c2367ea5e1bfc5e97159cf67edd05a431b65d
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-01 07:55:41 +0000

    datatypes and variable

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,16 @@
+const accountId =1345
+let accountEmail ="Dhawal.com"
+var accountPassword="345"
+ let accountCity="Jaipur"
+ let accountState;
+
+//accountId =2 //Not allowed
+
+accountEmail="pritesh.com"
+accountPassword="456"
+accountCity="indore"
+console.log(accountId);
+/* prefer not to use vsar because of issue
+in block scope and funtional scope*/
+ console.table([accountId,accountEmail,accountPassword, accountCity, accountState]);
+ 
