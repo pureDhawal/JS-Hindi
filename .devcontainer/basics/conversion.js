3e39650ab84d88854b0425358a017da1a259fad7
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-08 11:17:43 +0000

    conversion and operation

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,38 @@
+//let score="3dhawal"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueinnumber=Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+//let some=33;
+//let stringnumber=String(some);
+//console.log(stringnumber)
+//console.log(typeof stringnumber);
+
+//.             operations
+let number=3;
+let negvalue=-number;
+console.log(negvalue);
+
+
+let str1="Dhawal"
+let str2=" Hello"
+let str3=str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log(((3+4)*5)%3);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2;
+console.log(num1)
+console.log(num2)
+console.log(num3);
+let gamecounter=100;
+++gamecounter ;
+console.log(gamecounter);
---
+++ .devcontainer/basics/operation.js
@@ -0,0 +1,22 @@
+
+let num1,num2,num3
+num1=num2=num3=2+2;
+//console.log(num1)
+//console.log(num2)
+//console.log(num3);
+let gamecounter=100;
+++gamecounter ;
+//console.log(gamecounter);
+
+//console.log("2">1);
+//console.log(null>0);
+//console.log(null==0);
+//console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined >0);
+console.log(undefined <0);
+
+//===
+console.log("2"===2)
+console.log(2===2)
