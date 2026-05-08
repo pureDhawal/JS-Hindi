4281cb5508cbf2cbebd9160d2ee8e17e2c862913
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-08 14:59:25 +0000

    datatype-summary

---
+++ .devcontainer/basics/datatype-summary.js
@@ -0,0 +1,23 @@
+const score=100;
+const scorevalue=100.3
+
+const logedin=false;
+const outsidetemp=null;
+let userEmail;
+
+const id=Symbol('123');
+const anthorid=Symbol('123')
+
+console.log(id===anthorid);
+
+const heros=["shaktiman","paunchiman","naagraj"];
+let myobject={
+    name:"dhawal",
+    age: 23,
+}
+console.log(myobject);
+const myfunction=function(){
+    console.log("Hello world");
+}
+console.log(typeof myfunction);
+
