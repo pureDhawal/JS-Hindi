fa638782fcdf9481513ee22fca043122806ae61e
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-09 12:44:35 +0000

    Date and time function

---
+++ .devcontainer/basics/date_and_time.js
@@ -0,0 +1,12 @@
+let mydate=new Date();
+//console.log(mydate.toString());
+//console.log(mydate.toDateString());
+//console.log(mydate.toISOString());
+//console.log(mydate.toJSON());
+
+let mynewdate=new Date("03-24-2023");
+console.log(mynewdate.toLocaleString());
+
+let mytimestamp=Date .now();
+console.log(mytimestamp);
+console.log(mynewdate.getTime());
