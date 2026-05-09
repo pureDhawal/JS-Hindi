e0e8759e43a421cac49a8cd89b393029fc80320d
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-09 10:22:30 +0000

    String and its function

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,22 @@
+const name="Dhawal"
+const repocount=" 30 "
+
+console.log(name + repocount +"Value");
+
+const gamechanger= new String("Priyanshu")
+
+console.log(gamechanger[0]);
+console.log(gamechanger.__proto__);
+console.log(gamechanger.toUpperCase());
+console.log(gamechanger.length);
+console.log(gamechanger.charAt(4));
+console.log(gamechanger.indexOf("h"));
+const newstring=gamechanger.substring(0,5);
+console.log(newstring);
+const anotherstring=gamechanger.slice(-8,4);
+console.log(anotherstring);
+const newstring2="    prab      ";
+console.log(newstring2);
+console.log(newstring2.trim());
+const url="https://www.hitesh%20choudhary.com";
+console.log(url.replace("%20","-"));
