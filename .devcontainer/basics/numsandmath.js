c50a9b2558dda8fc40dd8000043f71b378c2c086
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-09 11:40:35 +0000

    number and Maths

---
+++ .devcontainer/basics/numsandmath.js
@@ -0,0 +1,29 @@
+const score=400;
+//console.log(score);
+const balance =new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const newnumber=123.898;
+//console.log(newnumber.toPrecision(4));
+
+//++++++++++++++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++++++//
+console.log(Math);
+console.log(Math.round(2.4));
+console.log(Math.abs(-6));
+console.log(Math.ceil(2.3));
+console.log(Math.floor(2.9));
+console.log(Math.min(3,2,6,1));
+console.log(Math.max(4,5,8,9));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
