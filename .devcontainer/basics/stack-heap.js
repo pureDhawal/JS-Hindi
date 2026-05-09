68cff45b12f37634d75b6eb880c122490704e1c3
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-09 10:05:15 +0000

    stack and heap memory

---
+++ .devcontainer/basics/stack-heap.js
@@ -0,0 +1,17 @@
+let myyoutube="puredhawal"
+
+let anothername=myyoutube;
+anothername="chai aur code ";
+console.log(myyoutube);
+console.log(anothername);
+
+
+let userone={
+    email:"dhawal@google.com",
+    upi:"use123"
+}
+
+let usertwo=userone;
+usertwo.email="chai@google.com";
+console.log(userone.email);
+console.log(usertwo.email);
