0d7b0a42699b953fb18922e820771d68cb77523d
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-13 12:19:19 +0000

    arrow function and this keyword

---
+++ .devcontainer/arrow.js
@@ -0,0 +1,16 @@
+//function chai(){
+  //  const username="Hitesh";
+    //console.log(this.username);
+//}
+//chai();
+
+//const chai = () =>{
+  //  let username="hitesh"
+    //console.log(this)
+//}
+
+//chai()
+
+(function(){
+console.log("DB Connected");
+})();
