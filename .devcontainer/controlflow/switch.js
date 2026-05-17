ede158379c0fa9fb00f46bca24e6204deb5c812f
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-17 13:13:38 +0000

    switch and true false condition

---
+++ .devcontainer/controlflow/switch.js
@@ -0,0 +1,14 @@
+const month=3;
+switch(month){
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("february")
+        break;
+   case 4:
+    console.log("March")
+    break;
+    default:
+        console.log("invalid month");
+}
---
+++ .devcontainer/controlflow/truthy.js
@@ -0,0 +1,16 @@
+const useremail= [];
+if(useremail.lenght===0){
+    console.log("usermail is empty");
+}
+const emptyobj={}
+if(Object.keys(emptyobj).length===0){
+    console.log("object is empty");
+}
+
+let val1;
+//val1=5 ?? 10;
+//val1=null ?? 10;
+//val1=undefined ?? 10;
+val=null ?? 10 ?? 20
+console.log(val1);
+///Ternary operator
