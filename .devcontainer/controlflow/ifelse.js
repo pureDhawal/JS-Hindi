a000d632b851197bd203538e0560af890bc5ae6d
DHAWAL GILKE <dhawalgilkenimcet@gmail.com>
2026-05-17 11:55:38 +0000

    conditional statement and usage of AND and OR operator

---
+++ .devcontainer/controlflow/ifelse.js
@@ -0,0 +1,25 @@
+const isuserloggedin=true;
+if(2=="2"){
+    console.log("Executed");
+}
+if(isuserloggedin){
+    console.log("Executed");
+}
+if(2==="2"){
+    console.log("Executed");
+}
+const temp=41;
+if(temp>50){
+    console.log("temperature is higher than 50");
+}
+else{
+    console.log("temperature is lower than 50");
+}
+const user=true;
+const debitcard=false;
+if(user && debitcard && 2==2){
+    console.log("Allow to buy the course");
+}
+if(user || debitcard ){
+    console.log("Allow to buy the course");
+}
